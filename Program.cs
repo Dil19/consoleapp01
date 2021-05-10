061031182c2d1b2b4069c525cc556511a5deaef8
Dil19 <59259863+Dil19@users.noreply.github.com>
2021-05-11 00:00:17 +0530

    heap and stack memory

--- Program.cs
+++ Program.cs
@@ -6,15 +6,17 @@ namespace ConsoleApp1HelloWorld
     {
         static void Main(string[] args)
         {
-            //postfix increment
-            int a = 1;
-            int b = a++;
+            //heap and stack memory
+            var a = 1;
+            var b = a;
+            b++;
             Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
 
-            //prefix increment
-            b = ++a;
-            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
-
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1: {0}, array2: {1}", array1, array2));
+            Console.WriteLine(string.Format("array1: {0}, array2: {1}", array1[0], array2[0]));
         }
     }
 }
